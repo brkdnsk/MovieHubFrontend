478a38bab578d8b77748f50b8241e0e988748871
brkdnsk <bburakdnsk@gmail.com>
2025-08-15 00:31:20 +0300

    Controller

--- src/main/java/com/moviehub/MovieHub/controller/MovieController.java
+++ src/main/java/com/moviehub/MovieHub/controller/MovieController.java
@@ -1,4 +1,18 @@
 package com.moviehub.MovieHub.controller;
 
+
+import com.moviehub.MovieHub.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("movies")
+@CrossOrigin(origins = "http://localhost:5500")
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
+
 }
