26090dce6ddf75f5b1ea88b9cdfdbdb0185ab5df
brkdnsk <bburakdnsk@gmail.com>
2025-08-28 04:36:07 +0300

    Fix repo: remove nested .git, add backend & frontend as regular folders

---
+++ MovieHub/src/main/java/com/moviehub/MovieHub/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.moviehub.MovieHub.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
